6569ea95af58c02b73abf0392fd2d51febdac29b
Erik <erik.daniel.nobleta@oocl.com>
2020-10-01 10:20:11 +0800

    Finished activity

---
+++ multiplication_table/index.js
@@ -0,0 +1,5 @@
+for(let x = 1; x <= 9; x++){
+    for (let y = 1; y <= x; y++){
+        console.log(x + " * " + y + " = " + (x*y));
+    }
+}
---
+++ number/index.js
@@ -0,0 +1,8 @@
+for(x = 1; x <= 20; x++){
+    if(x % 2 === 0){
+        console.log(x + " is an even number.");
+    }
+    else{
+        console.log(x + " is an odd number.");
+    }
+}
